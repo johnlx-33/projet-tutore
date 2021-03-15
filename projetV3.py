83bcbc43241867a4b563846c0e2e2f48e1c7d91d
john lavoix <john.lavoix@etu.u-bordeaux.fr>
2021-03-15 15:47:38 +0100

    ajout d'un fichier test + fonction distance de hamming

--- projetV3.py
+++ projetV3.py
@@ -44,12 +44,12 @@ def liste_nb_fact_premier(n):
 
 def generateur_de_cas(n,k):
     ## n>k renvoie 2 liste
-    ## liste des facteurs de n
-    ## liste de k mod facteur de n
-    ln = liste_nb_fact_premier(n)
-    lk = []
-    for i in range(len(ln)):
-        lk = lk + [k%ln[i]]
+    ## liste des facteurs de n l_modulo
+    ## liste de k mod facteur de n l_reste
+    l_modulo = liste_nb_fact_premier(n)
+    l_reste = []
+    for i in range(len(l_modulo)):
+        l_reste = l_reste + [k%l_modulo[i]]
     
     return (ln,lk)
 
