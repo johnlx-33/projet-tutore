3425f758f9e650ce824b3893052a83f58fad0b2a
john lavoix <john.lavoix@etu.u-bordeaux.fr>
2021-04-15 22:06:50 +0200

    test avec 51 nb premier 4 erreur

--- test.py
+++ test.py
@@ -20,7 +20,17 @@ L_modulo_11=[2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53]
 L_modulo_12=[2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59]
 L_modulo_13=[2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61]
 L_modulo_14=[2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67]
-L_modulo_15=[2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71]
+L_modulo_15=[2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71]   
+L_modulo_16=[2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73]  
+L_modulo_17=[2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79]  
+L_modulo_18=[2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83]
+L_modulo_19=[2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89]
+L_modulo_20=[2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97]
+L_modulo_21=[2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101]
+L_modulo_22=[2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103]
+L_modulo_23=[2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107]
+L_modulo_24=[2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113]
+L_modulo_25=[2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113,127,131,137,139,149,151,157,163,173,179,181,191,193,197,199,211,223,227,229,233,239]
 
 
 
@@ -31,7 +41,19 @@ d=858  ##[0, 0, 3, 4, 0, 0, 8]
 e=1453 ##[1, 1, 3, 4, 1, 10, 8]
 f=1958 ##[0, 2, 3, 5, 0, 8, 3]
 g=2300 ##[0, 1, 0, 1, 3, 4, 1]
-h=2500 ##
+h=2500          ##
+i=5000          ## [0, 2, 0, 2, 6, 8, 2, 3, 9, 12, 9, 5, 39, 12, 18, 18, 44, 59, 42, 30, 36, 23, 20, 16, 53, 51, 56
+                ##, 78, 95, 28, 47, 22, 68, 135, 83, 17, 133, 110, 156, 167, 113, 34, 175, 75, 25, 147, 94, 6, 191, 107, 220]
+j=10000         ## [0, 1, 0, 4, 1, 3, 4, 6, 18, 24, 18, 10, 37, 24, 36, 36, 29, 57, 17, 60,
+                ##  72, 46, 40, 32, 9, 1, 9, 49, 81, 56, 94, 44, 136, 131, 17, 34, 109, 57, 139, 155, 45, 68, 157, 150, 50, 83, 188, 12, 153, 214, 201]
+                
+k=15000         ##
+l=20000         ## 
+m=165165        ##
+n=6468468       ## 
+o=86842888231   ##
+
+
 ## test avec N=510510
 (aa,l_a)=generateur_de_cas(510510,a)
 (aa,l_b)=generateur_de_cas(510510,b)
@@ -40,14 +62,36 @@ h=2500 ##
 (aa,l_e)=generateur_de_cas(510510,e)
 (aa,l_f)=generateur_de_cas(510510,f)
 (aa,l_h)=generateur_de_cas(510510,h)
+
+
+## test avec 51 nb premier
+(bb,l_i)= generateur_de_cas_liste(L_modulo_25,i)
+(bb,l_j)= generateur_de_cas_liste(L_modulo_25,j)
+(bb,l_k)= generateur_de_cas_liste(L_modulo_25,k)
+(bb,l_l)= generateur_de_cas_liste(L_modulo_25,l)
+(bb,l_m)= generateur_de_cas_liste(L_modulo_25,m)
+(bb,l_n)= generateur_de_cas_liste(L_modulo_25,n)
+(bb,l_o)= generateur_de_cas_liste(L_modulo_25,o)
+
+
 ## test 1
+"""
 print(l_a)
 print(l_b)
 print(l_c)
 print(l_d)
 print(l_e)
 print(l_f)
-print(l_h)      
+print(l_h)
+print("")
+print(l_i)
+print(l_j)
+print(l_k)
+print(l_l)
+print(l_m)
+print(l_n)
+print(l_o)
+"""
 ## liste erroné
 
 ##1 erreur
@@ -82,9 +126,21 @@ lr_1f=[0, 1, 0, 1, 3, 4, 1]
 lr_1h=[0, 1, 0, 1, 3, 4, 2]
 lr_2h=[0, 1, 1, 1, 3, 4, 1]
 
+## lr_1i erreur pos 0,1 mod 2 3
+lr_1i=[1, 0, 0, 2, 6, 8, 2, 3, 9, 12, 9, 5, 39, 12, 18, 18, 44, 59, 42, 30, 36, 23, 20, 16, 53, 51, 56, 78, 95, 28, 47, 22, 68, 135, 83, 17, 133, 110, 156, 167, 113, 34, 175, 75, 25, 147, 94, 6, 191, 107, 220]
+## lr_2i erreur pos  2 5 mod 5 13
+lr_2i=[0, 2, 3, 2, 6, 3, 2, 3, 9, 12, 9, 5, 39, 12, 18, 18, 44, 59, 42, 30, 36, 23, 20, 16, 53, 51, 56, 78, 95, 28, 47, 22, 68, 135, 83, 17, 133, 110, 156, 167, 113, 34, 175, 75, 25, 147, 94, 6, 191, 107, 220]
+## lr_3i erreur pos  9 12 mod 29 41
+lr_3i=[0, 2, 0, 2, 6, 8, 2, 3, 9, 2, 9, 5,12, 12, 18, 18, 44, 59, 42, 30, 36, 23, 20, 16, 53, 51, 56, 78, 95, 28, 47, 22, 68, 135, 83, 17, 133, 110, 156, 167, 113, 34, 175, 75, 25, 147, 94, 6, 191, 107, 220]
+
+## lr_4i erreur pos 2 9 12 mod 5 29 41
+lr_4i=[0, 2, 1, 2, 6, 8, 2, 3, 9, 2, 9, 5,12, 12, 18, 18, 44, 59, 42, 30, 36, 23, 20, 16, 53, 51, 56, 78, 95, 28, 47, 22, 68, 135, 83, 17, 133, 110, 156, 167, 113, 34, 175, 75, 25, 147, 94, 6, 191, 107, 220]
+
+## lr_5i erreur pos 2 9 12 15 mod 5 29 41 53
+lr_5i=[0, 2, 1, 2, 6, 8, 2, 3, 9, 2, 9, 5,12, 12, 18, 30, 44, 59, 42, 30, 36, 23, 20, 16, 53, 51, 56, 78, 95, 28, 47, 22, 68, 135, 83, 17, 133, 110, 156, 167, 113, 34, 175, 75, 25, 147, 94, 6, 191, 107, 220]
 
 
-## test avec N=510510
+## test avec N=510
 
 """
 print("lr_1a",détection_1_erreur(lr_1a,aa))
@@ -178,11 +234,71 @@ print("")
 print("test sur 547")
 print("")
 
-print(decode_fraction_continu(lr_1c,aa,1))
-print(decode_fraction_continu(lr_2c,aa,1))
-print(decode_fraction_continu(lr_3c,aa,1))
-print(decode_fraction_continu(lr_4c,aa,1))
-print(decode_fraction_continu(lr_5c,aa,1))
+print("n-uplet corespondant <->"+str(lr_1c))
+tmp=decode_fraction_continu(lr_1c,aa,1)
+print(tmp)
+print(recontitution_fraction(tmp,lr_1c,aa))
+print("")
+
+print("n-uplet corespondant <->"+str(lr_2c))
+tmp=decode_fraction_continu(lr_2c,aa,1)
+print(tmp)
+print(recontitution_fraction(tmp,lr_2c,aa))
+print("")
+
+print("n-uplet corespondant <->"+str(lr_3c))
+tmp=decode_fraction_continu(lr_3c,aa,1)
+print(tmp)
+print(recontitution_fraction(tmp,lr_3c,aa))
+print("")
+
+print("n-uplet corespondant <->"+str(lr_4c))
+tmp=decode_fraction_continu(lr_4c,aa,1)
+print(tmp)
+print(recontitution_fraction(tmp,lr_4c,aa))
+print("")
+
+print("n-uplet corespondant <->"+str(lr_5c))
+tmp=decode_fraction_continu(lr_5c,aa,1)
+print(tmp)
+print(recontitution_fraction(tmp,lr_5c,aa))
+print("")
+
+print("essaie avec 51 nb premier")
+print("")
+print("n-uplet corespondant <->"+str(lr_1i))
+tmp=decode_fraction_continu(lr_1i,bb,2)
+print(tmp)
+
+
+print("")
+print("n-uplet corespondant <->"+str(lr_2i))
+tmp=decode_fraction_continu(lr_2i,bb,2)
+print(tmp)
+
+
+print("")
+print("n-uplet corespondant <->"+str(lr_3i))
+tmp=decode_fraction_continu(lr_3i,bb,2)
+print(tmp)
+
+
+print("")
+print("essaie 3 erreur avec 51 nb premier")
+print("")
+print("n-uplet corespondant <->"+str(lr_4i))
+tmp=decode_fraction_continu(lr_4i,bb,5)
+print(tmp)
+
+
+
+print("")
+print("essaie 4 erreur avec 51 nb premier")
+print("")
+print("n-uplet corespondant <->"+str(lr_5i))
+tmp=decode_fraction_continu(lr_5i,bb,5)
+print(tmp)
+
 
 print("")
 
